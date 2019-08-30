fdf43a3664e13c1c9ab3f1465bf7e7088fc03ef5
donrestarone <shashikejayatunge@gmail.com>
2019-08-30 17:44:29 -0400

    add logic to live stream messages

--- room/src/Components/Containers/Room/Room.js
+++ room/src/Components/Containers/Room/Room.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {showRoom} from '../../../Services/rooms'
 import {createMessage} from '../../../Services/messages'
 import MessageListItem from '../../List/MessageListItem/MessageListItem'
+import {getApiWebsocketRoot} from '../../../Constants/Api'
+// import { ActionCableProvider, ActionCable } from 'react-actioncable-provider';
+import MessagesHolder from '../../../Components/Room/MessagesHolder/MessagesHolder'
+import ActionCable from 'actioncable'
 import './Room.css'
 class Room extends Component {
 
@@ -10,7 +14,20 @@ class Room extends Component {
     roomName: null,
     message: ''
   }
+  componentWillMount = () => {
+    let roomId = this.props.match.params.id
+    this.initializeActionCable(roomId)
+  }
 
+  initializeActionCable = (roomId) => {
+    let cable = ActionCable.createConsumer(getApiWebsocketRoot())
+    cable.subscriptions.create(
+      { channel: 'MessagesChannel', roomId: roomId },
+      { received: (data) => { this.handleReceivedMessage(data) } },
+      { connected: () => { console.log('connected')} },
+      { disconnect: () => { console.log('disconnected')} },
+    )
+  }
   handleMessageInput = (e) => {
     let input = e.target.value
     this.setState({message: input})
@@ -36,11 +53,19 @@ class Room extends Component {
     .then(object => {
       if (object.data) {
         let message = object.data
-        this.setState(prevState => ({
-          messages: [...prevState.messages, message]
-        }))
+        this.updateMessageListState(message)
       }
     })
+  }
+
+  updateMessageListState = (newMessage) => {
+    let existingMessages = this.state.messages
+    let messageExists = existingMessages.some(message => message.id === newMessage.id)
+    if (!messageExists) {
+      this.setState(prevState => ({
+        messages: [...prevState.messages, newMessage]
+      }))
+    }
   }
 
   componentDidMount = () => {
@@ -76,20 +101,26 @@ class Room extends Component {
       return roomName
     }
   }
+
+  handleReceivedMessage = (object) => {
+    let message = object.data
+    this.updateMessageListState(message)
+  }
+
+  showRoomName = () => {
+    let roomName = this.state.roomName
+    if (roomName) {
+      return roomName
+    }
+  }
   render() {
     return (
-    <div className="room-wrapper"> 
-      <div className="room-title-wrapper">
-        <h1 className="room-title">{this.showRoomName()}</h1>
-      </div>
-      <div className="messages-wrapper">
-        {this.showMessages()}
-      </div>
-      <div className="input-wrapper">
-        <input type="text" onChange={this.handleMessageInput} value={this.state.message} onKeyUp={this.handleMacroKeyInput}></input>
-      </div>
-    </div>
-    
+      <>
+        <div className="room-title-wrapper">
+          <h1 className="room-title">{this.showRoomName()}</h1>
+        </div>
+        <MessagesHolder handleInput={this.handleMessageInput} handleMacroKeyInput={this.handleMacroKeyInput} message={this.state.message} messages={this.state.messages}></MessagesHolder>
+      </>
     );
   }
 }
