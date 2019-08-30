2cf4bb8f42c4cc37b29fa3f8fb9a8d153a20edd5
donrestarone <shashikejayatunge@gmail.com>
2019-08-30 17:45:03 -0400

    import websocket path returning function

--- room/src/Services/messages.js
+++ room/src/Services/messages.js
@@ -1,4 +1,4 @@
-import {getApiRoot} from '../Constants/Api'
+import {getApiRoot, getApiWebsocketRoot} from '../Constants/Api'
 
 export const createMessage = (roomId, messageBody) => {
   return new Promise((resolve, reject) => {
