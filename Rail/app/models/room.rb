bb08101bb37d7952f540fc654077b3255d06f2de
donrestarone <shashikejayatunge@gmail.com>
2019-09-02 12:25:33 -0400

    add methods to securely generate a unique room name

--- Rail/app/models/room.rb
+++ Rail/app/models/room.rb
@@ -1,3 +1,24 @@
 class Room < ApplicationRecord
   has_many :messages
+  validates :slug, uniqueness: true, presence: true
+
+  def self.handle_creation(room_name)
+    room = Room.new
+    room.generate_slug
+    room.name = room_name
+    if room.save
+      return room
+    else  
+      return false
+    end
+  end
+
+  def generate_slug
+    generated_slug = SecureRandom.hex(10)
+    if Room.find_by(id: generated_slug)
+      self.generate_slug
+    else 
+      return self.slug = generated_slug
+    end
+  end 
 end
