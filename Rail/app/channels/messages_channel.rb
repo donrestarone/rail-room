8819c578e2417297dc8a05372d05144ae61a6f7c
donrestarone <shashikejayatunge@gmail.com>
2019-09-02 16:45:56 -0400

    increment and decrement user count upon connection/disconnection

--- Rail/app/channels/messages_channel.rb
+++ Rail/app/channels/messages_channel.rb
@@ -2,10 +2,13 @@ class MessagesChannel < ApplicationCable::Channel
   def subscribed
     room_id = params["roomId"]
     room = Room.includes(:messages).find_by(slug: room_id)
+    room.update(users_count: room.users_count + 1)
     stream_for room
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    room_id = params["roomId"]
+    room = Room.includes(:messages).find_by(slug: room_id)
+    room.update(users_count: room.users_count - 1)
   end
 end
