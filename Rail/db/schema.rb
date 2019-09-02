bc3c8d76d245445bd351fac0857826d6a4a72ac7
donrestarone <shashikejayatunge@gmail.com>
2019-09-02 16:46:55 -0400

    schema file

--- Rail/db/schema.rb
+++ Rail/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_162809) do
+ActiveRecord::Schema.define(version: 2019_09_02_201137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_162809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "users_count"
     t.index ["slug"], name: "index_rooms_on_slug"
   end
 
