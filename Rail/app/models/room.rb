9e579a9add361eb3f735bb544c4cad45a4c66ed1
donrestarone <shashikejayatunge@gmail.com>
2019-09-02 16:46:21 -0400

    initialize default value

--- Rail/app/models/room.rb
+++ Rail/app/models/room.rb
@@ -2,8 +2,10 @@ class Room < ApplicationRecord
   has_many :messages
   validates :slug, uniqueness: true, presence: true
 
+  after_update_commit { RoomBroadcastJob.perform_later(self) }
+
   def self.handle_creation(room_name)
-    room = Room.new
+    room = Room.new(users_count: 0)
     room.generate_slug
     room.name = room_name
     if room.save
