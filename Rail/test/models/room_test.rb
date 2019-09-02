a964a575dfd60ea720fd043bfb376eab6387fd78
donrestarone <shashikejayatunge@gmail.com>
2019-09-02 12:26:04 -0400

    add unit test/

--- Rail/test/models/room_test.rb
+++ Rail/test/models/room_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "room slugs are unique & secure" do
+    1000.times do 
+      room_name = SecureRandom.hex(10)
+      room = Room.handle_creation(room_name)
+      byebug
+      assert(room)
+    end
+  end
 end
